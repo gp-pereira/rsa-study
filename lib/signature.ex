3f02c05707b56b9a3d013e2c4b5aee7f36b7455c
gp-pereira <pereira7346@gmail.com>
2022-08-01 16:04:17 -0300

    refactor: added string contains to signature

--- lib/signature.ex
+++ lib/signature.ex
@@ -3,8 +3,13 @@ defmodule RSA.Signature do
     do: RSA.encrypt_message(original_signature, private_key)
 
   def verify(signature, original_signature, public_key) do
-    if RSA.decrypt_message(signature, public_key) == original_signature,
-      do: :ok,
-      else: :error
+    signature
+    |> RSA.decrypt_message(public_key)
+    |> String.contains?(original_signature)
+    |> if do
+      :ok
+    else
+      :error
+    end
   end
 end
